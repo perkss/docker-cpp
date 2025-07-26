63c7301948f1f6dd082cd4d44b1fd68f5a6c015a
perkss <stuart.m.perks@gmail.com>
2025-07-26 11:45:20 +0100

    add in update container

--- tests/update_container_cmd_test.cc
+++ tests/update_container_cmd_test.cc
@@ -2,6 +2,7 @@
 #include <memory>
 
 #include "update_container_cmd.hh"
+#include "update_container_cmd_exec.hh"
 #include "docker_client.hh"
 
 using namespace dockercpp::command;
@@ -17,56 +18,56 @@ protected:
     }
 };
 
-// TEST_F(UpdateContainerCmdTest, TestUpdateContainerCmdBasicProperties) {
-//     std::unique_ptr<update::Exec> exec = std::make_unique<update::Exec>();
-//     UpdateContainerCmdImpl cmd(std::move(exec));
+TEST_F(UpdateContainerCmdTest, TestUpdateContainerCmdBasicProperties) {
+    std::unique_ptr<update::Exec> exec = std::make_unique<dockercpp::command::exec::UpdateContainerCmdExec>();
+    UpdateContainerCmdImpl cmd(std::move(exec));
 
-//     std::string testContainerId = "test_container_123";
-//     cmd.withContainerId(testContainerId);
+    std::string testContainerId = "test_container_123";
+    cmd.withContainerId(testContainerId);
 
-//     EXPECT_EQ(cmd.getContainerId(), testContainerId);
-// }
+    EXPECT_EQ(cmd.getContainerId(), testContainerId);
+}
 
-// TEST_F(UpdateContainerCmdTest, TestUpdateContainerCmdResourceLimits) {
-//     std::unique_ptr<update::Exec> exec = std::make_unique<update::Exec>();
-//     UpdateContainerCmdImpl cmd(std::move(exec));
+TEST_F(UpdateContainerCmdTest, TestUpdateContainerCmdResourceLimits) {
+    std::unique_ptr<update::Exec> exec = std::make_unique<dockercpp::command::exec::UpdateContainerCmdExec>();
+    UpdateContainerCmdImpl cmd(std::move(exec));
 
-//     int testCpuShares = 512;
-//     int64_t testMemory = 1024 * 1024 * 1024; // 1GB
-//     int testMemorySwap = 2048;
+    int testCpuShares = 512;
+    int64_t testMemory = 1024 * 1024 * 1024; // 1GB
+    int testMemorySwap = 2048;
 
-//     cmd.withCpuShares(testCpuShares)
-//        .withMemory(testMemory)
-//        .withMemorySwap(testMemorySwap);
+    cmd.withCpuShares(testCpuShares)
+       .withMemory(testMemory)
+       .withMemorySwap(testMemorySwap);
 
-//     EXPECT_EQ(cmd.getCpuShares(), testCpuShares);
-//     EXPECT_EQ(cmd.getMemory(), testMemory);
-//     EXPECT_EQ(cmd.getMemorySwap(), testMemorySwap);
-// }
+    EXPECT_EQ(cmd.getCpuShares(), testCpuShares);
+    EXPECT_EQ(cmd.getMemory(), testMemory);
+    EXPECT_EQ(cmd.getMemorySwap(), testMemorySwap);
+}
 
-// TEST_F(UpdateContainerCmdTest, TestUpdateContainerCmdCpuSet) {
-//     std::unique_ptr<update::Exec> exec = std::make_unique<update::Exec>();
-//     UpdateContainerCmdImpl cmd(std::move(exec));
+TEST_F(UpdateContainerCmdTest, TestUpdateContainerCmdCpuSet) {
+    std::unique_ptr<update::Exec> exec = std::make_unique<dockercpp::command::exec::UpdateContainerCmdExec>();
+    UpdateContainerCmdImpl cmd(std::move(exec));
 
-//     std::vector<std::string> testCpusetCpus = {"0", "1", "2"};
-//     cmd.withCpusetCpus(testCpusetCpus);
+    std::vector<std::string> testCpusetCpus = {"0", "1", "2"};
+    cmd.withCpusetCpus(testCpusetCpus);
 
-//     EXPECT_EQ(cmd.getCpusetCpus(), testCpusetCpus);
-// }
+    EXPECT_EQ(cmd.getCpusetCpus(), testCpusetCpus);
+}
 
-// TEST_F(UpdateContainerCmdTest, TestUpdateContainerCmdChaining) {
-//     std::unique_ptr<update::Exec> exec = std::make_unique<update::Exec>();
-//     UpdateContainerCmdImpl cmd(std::move(exec));
+TEST_F(UpdateContainerCmdTest, TestUpdateContainerCmdChaining) {
+   std::unique_ptr<update::Exec> exec = std::make_unique<dockercpp::command::exec::UpdateContainerCmdExec>();
+     UpdateContainerCmdImpl cmd(std::move(exec));
 
-//     std::string testContainerId = "test_container_123";
-//     int testCpuShares = 256;
-//     int64_t testMemory = 512 * 1024 * 1024;
+    std::string testContainerId = "test_container_123";
+    int testCpuShares = 256;
+    int64_t testMemory = 512 * 1024 * 1024;
 
-//     cmd.withContainerId(testContainerId)
-//        .withCpuShares(testCpuShares)
-//        .withMemory(testMemory);
+    cmd.withContainerId(testContainerId)
+       .withCpuShares(testCpuShares)
+       .withMemory(testMemory);
 
-//     EXPECT_EQ(cmd.getContainerId(), testContainerId);
-//     EXPECT_EQ(cmd.getCpuShares(), testCpuShares);
-//     EXPECT_EQ(cmd.getMemory(), testMemory);
-// }
+    EXPECT_EQ(cmd.getContainerId(), testContainerId);
+    EXPECT_EQ(cmd.getCpuShares(), testCpuShares);
+    EXPECT_EQ(cmd.getMemory(), testMemory);
+}
