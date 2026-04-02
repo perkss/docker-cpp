c0b92ea63e016bcc1f0944683e4966d38d7484f2
perkss <stuart.m.perks@gmail.com>
2026-04-02 02:46:58 +0100

    Remove unnecessary installation scripts and configuration files for docker-cpp project. This includes deleting cmake_install.cmake, docker_cpp.pc, docker_cppConfig.cmake, docker_cppConfigVersion.cmake, CTestTestfile.cmake, and src/cmake_install.cmake to streamline the build process and eliminate unused components.

--- build-test/_deps/json-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Stuart/Documents/Programming/C++/docker-cpp/build-test/_deps/json-src
-# Build directory: /Users/Stuart/Documents/Programming/C++/docker-cpp/build-test/_deps/json-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
